b9270b239c2fc0b305a1285d824e25ed36430c68
JulieDelts <juliadelcova@gmail.com>
2025-02-12 01:50:42 +0300

    Fix validation

--- CustomersService.Presentation/Configuration/ServicesConfiguration.cs
+++ CustomersService.Presentation/Configuration/ServicesConfiguration.cs
@@ -1,10 +1,15 @@
-﻿
+﻿using CustomersService.Presentation.Models.Requests.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
 namespace CustomersService.Presentation.Configuration
 {
     public static class ServicesConfiguration
     {
         public static void AddPresentationServices(this IServiceCollection services)
         {
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
         }
     }
 }
---
+++ CustomersService.Presentation/Models/Requests/Validators/PasswordUpdateRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CustomersService.Presentation.Models.Requests.Validators
+{
+    public class PasswordUpdateRequestValidator: AbstractValidator<PasswordUpdateRequest>
+    {
+        public PasswordUpdateRequestValidator() 
+        {
+            RuleFor(model => model.NewPassword).NotNull().Length(8, 15);
+            RuleFor(model => model.CurrentPassword).NotNull().Length(8, 15);
+        }
+    }
+}
