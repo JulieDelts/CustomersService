cea34b722cc3a0181a0c65fb8af2dd0173eac53b
Malinin <kickassdom@gmail.com>
2025-02-12 16:12:39 +0300

    Configured json and added serilog packages to repos. Also added removing all logs from repo via gitignore

---
+++ CustomersService.Presentation/Configuration/LoggingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace CustomersService.Presentation.Configuration;
+internal static class LoggingConfiguration
+{
+    public static void ConfigureCustomLogging(this IHostBuilder host)
+    {
+        host.UseSerilog((context, configuration) =>
+        {
+            configuration
+                .ReadFrom.Configuration(context.Configuration);
+        });
+    }
+}
