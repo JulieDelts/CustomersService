cb83892d970f8db5d208ae50ca6bf52faab0f043
Malinin <kickassdom@gmail.com>
2025-02-12 10:40:12 +0300

    Added tests for account controller also maked some fixes for them(like mapping,response model)

---
+++ CustomersService.Presentation.Tests/TestCases/AccountControllerTestCases.cs
@@ -0,0 +1,57 @@
+﻿using CustomersService.Application.Models;
+using CustomersService.Core.Enum;
+using CustomersService.Presentation.Models.Requests;
+
+namespace CustomersService.Presentation.Tests.TestCases;
+
+internal static class AccountControllerTestCases
+{
+    public static IEnumerable<object[]> Accounts()
+    {
+        var accountModels = new List<AccountInfoModel>()
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                CustomerId = Guid.NewGuid(),
+                Currency = Currency.USD,
+                IsDeactivated = false
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                CustomerId = Guid.NewGuid(),
+                Currency = Currency.EUR,
+                IsDeactivated = false
+            }
+        };
+
+        yield return new object[] { accountModels };
+    }
+
+    public static IEnumerable<object[]> AccountToCreate()
+    {
+        var accountRequest = new AccountAddRequest()
+        {
+            CustomerId = Guid.NewGuid(),
+            Currency = Currency.USD
+        };
+
+        yield return new object[] { accountRequest };
+    }
+
+    public static IEnumerable<object[]> AccountFullInfo()
+    {
+        var accountFullInfo = new AccountFullInfoModel()
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = Guid.NewGuid(),
+            Currency = Currency.USD,
+            IsDeactivated = false,
+            Balance = 1000,
+            DateCreated = DateTime.Now
+        };
+
+        yield return new object[] { accountFullInfo };
+    }
+}
--- CustomersService.Presentation/Models/Responses/AccountResponse.cs
+++ CustomersService.Presentation/Models/Responses/AccountResponse.cs
@@ -1,11 +1,13 @@
-﻿namespace CustomersService.Presentation.Models.Responses
+﻿using CustomersService.Core.Enum;
+
+namespace CustomersService.Presentation.Models.Responses
 {
     public class AccountResponse
     {
         public Guid Id { get; set; }
         public Guid CustomerId { get; set; }
         public DateTime DateCreated { get; set; }
-        public string Currency { get; set; }
+        public Currency Currency { get; set; }
         public bool IsDeactivated { get; set; } 
     }
 }
